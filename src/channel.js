6c0e0b7c244967f0da80dca016d3f125ed3f2741
Ivan <z5421133@ad.unsw.edu.au>
2022-06-07 21:10:28 +1000

    Added channelDetailsV1 stub to channel.js

--- src/channel.js
+++ src/channel.js
@@ -1,4 +1,9 @@
+// Stub for channelDetailsV1 function 
+function channelDetailsV1(authUserId, channelId) { 
+    return 'authUserId' + 'channelId'; 
+}
+
 // Stub for channelMessagesV1 function 
 function channelMessagesV1(authUserId, channelId, start) { 
-    return 'authUserId ' + 'channelId ' + 'start '; 
+    return 'authUserId' + 'channelId' + 'start'; 
 }
