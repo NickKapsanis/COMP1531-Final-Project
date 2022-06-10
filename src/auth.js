b56c3b5351f47e97f3d906a9c0cea320306d09d8
Manav <z5417834@unsw.edu.au>
2022-06-10 17:17:36 +1000

    this is the first stub branch done

--- src/auth.js
+++ src/auth.js
@@ -2,8 +2,3 @@
 authLoginV1(email, password) {
     return 'email' + 'password';
 }
-
-//this is second authentication, will add more code here eventually.
-authRegisterV1(email, password, nameFirst, nameLast) {
-    return 'email' + 'password' + 'nameFirst' + 'nameLast';
-}
