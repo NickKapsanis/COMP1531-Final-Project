1de94ac19e8a8e5b806e9ad3829918c47c4f62fc
Giuliana De Bellis <z5309331@unsw.edu.au>
2022-06-11 16:17:44 +1000

    Results push

--- src/auth.js
+++
@@ -1,9 +0,0 @@
-//this is first authentication, will add more code here eventually.
-function authLoginV1(email, password) {
-    return 'email' + 'password';
-}
-
-//this is second authentication, will add more code here eventually.
-function authRegisterV1(email, password, nameFirst, nameLast) {
-    return 'email' + 'password' + 'nameFirst' + 'nameLast';
-}
