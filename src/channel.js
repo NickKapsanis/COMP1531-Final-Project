ffac30d912a0d74ee73e6b7df5fd06833979cb78
Ivan <z5421133@ad.unsw.edu.au>
2022-06-20 06:51:59 +1000

    Completed the implementation of channelDetailsV1

--- src/channel.js
+++ src/channel.js
@@ -1,7 +1,29 @@
+import {getData, setData} from './dataStore'; 
+import {channelsListV1, channelsListallV1 } from './channels.js'
+
 // Stub for channelDetailsV1 function 
-function channelDetailsV1(authUserId, channelId) { 
-    return 'authUserId' + 'channelId'; 
+function channelDetailsV1(authUserId, channelId) {
+    const store = getData(); 
+    const channelsMemberOf = channelsListV1(authUserId); 
+
+    if (store.channels.find(channel => channel.channelId === channelId) === undefined) {
+        return { error: 'error' }; 
+        
+    } else if (channelsMemberOf.find(channel => channel.channelId === channelId) === undefined) {
+        return { error: 'error' }; 
+    } 
+    
+    const channelDetails = store.channels.find(channel => channel.channelId === channelId); 
+
+    return {
+        name: channelDetails.name, 
+        isPublic: channelDetails.isPublic,
+        ownerMembers: [],
+        allMembers: channelDetails.users,
+    };
 }
+
+console.log(channelDetailsV1(72, 30));
 
 // Stub for channelMessagesV1 function 
 function channelMessagesV1(authUserId, channelId, start) { 
@@ -17,3 +39,5 @@ function channelJoinV1(authUserId, channelId) {
 function channelInviteV1(authUserId, channelId, uId) {
     return ('authUserId' + 'channelId' + 'uId');
 }
+
+export { channelDetailsV1, channelJoinV1, channelInviteV1, channelMessagesV1 };
