37dcd5a0d5bfeef9253015178a3bcf0318f1e2e3
Nick Kapsanis <z5254990@ad.unsw.edu.au>
2022-06-20 20:43:26 +1000

    created auth.test.js in src and implemented testing for authRegisterV1

--- src/auth.js
+++ src/auth.js
@@ -1,9 +1,12 @@
-//this is first authentication, will add more code here eventually.
+//authLogin V1
+//given a registered users email and password, return their authUserId.
 function authLoginV1(email, password) {
     return 'email' + 'password';
 }
 
-//this is second authentication, will add more code here eventually.
+//authRegisterV1
+//given users first annd last name, email and password, create a new account and return new authUserId
+//generate a handle for the user according to specifictions in interface.
 function authRegisterV1(email, password, nameFirst, nameLast) {
     return 'email' + 'password' + 'nameFirst' + 'nameLast';
 }
---
+++ src/auth.test.js
@@ -0,0 +1,47 @@
+import { authRegisterV1, authLoginV1 } from './auth';
+
+describe('Testing authRegisterV1 for input Error' , () => {
+    test.each([
+      {email: 'notanemail.com', password: '1234567', nameFirst: 'James', nameLast: 'Bond'},
+      {email: 'ThisisAnEmail@gmail.com', password: 'sub6', nameFirst: 'James', nameLast: 'Bond'},
+      {email: 'ThisisAnEmail@gmail.com', password: '1234567', nameFirst: '', nameLast: 'Bond'},
+      {email: 'ThisisAnEmail@gmail.com', password: '1234567', nameFirst: 'James', nameLast: ''},
+      {email: 'ThisisAnEmail@gmail.com', password: '1234567',
+       nameFirst: 'James012345678901234567890123456789012345678901234567890',
+        nameLast: 'Bond'},
+        {email: 'ThisisAnEmail@gmail.com', password: '1234567',
+       nameFirst: 'James',
+        nameLast: 'Bond012345678901234567890123456789012345678901234567890'},
+    ]) ('authRegisterV1($email , $password, $nameFirst, $nameLast)', (
+        {
+        email,
+        password,
+        nameFirst,
+        nameLast,
+    }
+    ) => {
+      expect(authRegisterV1(email, password, nameFirst, nameLast)).toStrictEqual({ error: 'error' });
+    });
+  }
+  )
+
+  describe('Testing authRegisterV1 for string handle output' , () => {
+    test.each([
+        {email: 'ThisisAnEmail@gmail.com', password: '1234567', nameFirst: 'James', nameLast: 'Bond', handle: 'jamesbond'},
+        {email: 'ThisisAnEmail@gmail.com', password: '1234567', nameFirst: 'J', nameLast: 'Bond', handle: 'jbond'},
+        {email: 'ThisisAnEmail@gmail.com', password: '1234567', nameFirst: 'James', nameLast: 'Bond007', handle: 'jamesbond007'},
+      ]) ('authRegisterV1($email , $password, $nameFirst, $nameLast)', (
+          {
+          email,
+          password,
+          nameFirst,
+          nameLast,
+          handle,
+      }
+      ) => {
+        expect(authRegisterV1(email, password, nameFirst, nameLast)).toStrictEqual(handle);
+      });
+
+  }
+  )
+
