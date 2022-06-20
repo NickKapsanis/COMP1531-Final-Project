7c1cf6d219c08900f9b2511d1dc89ad4ec1cdd81
Manav <z5417834@unsw.edu.au>
2022-06-21 01:30:29 +1000

    implemented the channelsListV1 function, however the testing relies on other functions, so it is untested.

--- src/channels.js
+++ src/channels.js
@@ -1,12 +1,46 @@
+import {getData, setData} from './dataStore.js';
 
-// Stub for channelsListV1 function
-function channelsListV1(authUserId) {
-    return 'authUserId';
+
+//this function gives an array of all public channels the given user is in
+export function channelsListV1(authUserId) {
+    const data = getData();
+
+    var numPublicChannels = 0;
+
+    //this finds the required array size.
+    for (var i = 0; i !== -1; i++) {
+        if (data.channels[i].isPublic === true) {
+            for (var j = 0; j !== -1; j++) {
+                if (data.channels[j].ownerMembers === authUserId) {
+                    numPublicChannels++;
+                }
+            }
+        }
+    }
+    
+    const channelsArray = array(numPublicChannels);
+    var k = 0;
+
+    //this loop finds a public channel,
+    //then searches for given user id within that channel.   
+    for (var i = 0; i !== -1; i++) {
+        if (data.channels[i].isPublic === true) {
+            for (var j = 0; j !== -1; j++) {
+                if (data.channels[j].ownerMembers === authUserId) {
+                    array[k] = data.channels[j];
+                    k++;
+                }
+            }
+        }
+    }
+
+    return channelsArray;
 }
 
-// Stub for channelsListAllV1 function
-function channelsListallV1(authUserId) {
-    return 'authUserId';
+//this function gives an array of all channels the given user is in
+export function channelsListallV1(authUserId) {
+
+    return channelsArray;
 }
 
 // Stub for channelsCreateV1 function
