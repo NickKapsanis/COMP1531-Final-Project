0b5cc28a383a1daabdbbf6e44d5fd2fc21618ca2
Ivan <z5421133@ad.unsw.edu.au>
2022-06-21 00:15:00 +1000

    Completed implementation of function, wrote documentation and added comments

--- src/channel.js
+++ src/channel.js
@@ -1,13 +1,31 @@
+import {getData, setData} from './dataStore.js'; 
+import {channelsListV1, channelsListallV1 } from './channels.js'
+
 // Stub for channelDetailsV1 function 
 function channelDetailsV1(authUserId, channelId) { 
     return 'authUserId' + 'channelId'; 
 }
 
-// Stub for channelMessagesV1 function 
+/**
+ * Returns up to 50 of user's most recent messages in given channel 
+ * and a specified range. 
+ * 
+ * Arguments: 
+ *      authUserId: integer     The user's unique identifier
+ *      channelId:  integer     The channel's unique identifier 
+ *      start:      integer     The left-bound of range 
+ *
+ * Returns:
+ *      {error: 'error'}     object     Error message when given invalid input
+ *      messageDetails       object     Object containing {messages, start, end}, 
+ *                                      where messages is an array of objects, 
+ *                                      start and end are integers. 
+ */ 
 function channelMessagesV1(authUserId, channelId, start) {
     const store = getData(); 
-    const channelsMemberOf = channelsListV1(authUserId); 
+    const channelsMemberOf = channelsListV1(authUserId);  
 
+    // Checking validity of 'channelId' input 
     if (store.channels.find(channel => channel.channelId === channelId) === undefined) {
         return { error: 'error' }; 
     } else if (channelsMemberOf.find(channel => channel.channelId === channelId) === undefined) {
@@ -16,27 +34,25 @@ function channelMessagesV1(authUserId, channelId, start) {
 
     const channelGiven = store.channels.find(channel => channel.channelId === channelId);
 
+    // Checking validity of 'start' input 
     let maxRecentIndex = false; 
     if (start > channelGiven.messages.length) {
         return { error: 'error' };
     } else if (start + 50 > channelGiven.messages.length) {
         maxRecentIndex = true; 
     }
-
+    
+    // Creating object to contain messages and the specified range 
     const messageDetails = {};     
     messageDetails.messages = channelGiven.messages.slice(start, start + 50); 
-
+    messageDetails.start = start; 
     if (maxRecentIndex === true) {
         messageDetails.end = -1;
     } else {
         messageDetails.end = start + 50;
     }
-    
-    return {
-      messages: [],
-      start: 0,
-      end: -1,
-    };
+
+    return messageDetails;
 }
 
 // Stub for channelJoinV1 function
