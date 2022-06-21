095005bb028ac8a2ae648e8da5559c2f7fa039e1
Ansh Deo Singh <anshdeosingh@gmail.com>
2022-06-21 15:08:28 +1000

    Updated testing for channels.js and modified dataStore

--- src/channels.test.js
+++ src/channels.test.js
@@ -1,18 +1,22 @@
 import { channelsListV1, channelsListallV1,channelsCreateV1 } from './channels.js';
 
+// Tests for channelsListV1() - Manav 
+
+
+// Tests for channelsListAllV1() - Manav
+
+
 // Tests for channelsCreateV1
-test('Test successful echo', () => {
-  let result = echo('1');
-  expect(result).toBe('1');
-  result = echo('abc');
-  expect(result).toBe('abc');
+test('Tests for channelsCreateV1', () => {
+  // Testing if error is returned when authUserId does not exist
+  // Testing if error is returned when name length < 1 or > 20 chars
+  // Testing if valid number is given as channel Id
+  // Testing if dataStore has new channel  
+  // Testing if user channels include new channel
+  // Testing if channels includes user  
 });
 
 test('Test invalid echo', () => {
   expect(echo({ echo: 'echo' })).toStrictEqual({ error: 'error' });
 });
 
-// Tests for channelsListV1()
-
-
-// Tests for channelsListAllV1()
--- src/dataStore.js
+++ src/dataStore.js
@@ -1,35 +1,7 @@
 // YOU SHOULD MODIFY THIS OBJECT BELOW
 let data = {
-  users : [
-    { 
-        'uid': 1,
-        'authUserId': 1000,
-        'nameFirst': 'SampleFirstName',
-        'nameLast': 'SampleLastName',
-        'email': 'sampleemail@email.com',
-        'password' : 'P@$$1234',
-        'handleStr': 'sampleUser11',
-        'channels': [10],
-    },
-    
-  ],  
-  channels : [
-    {
-        'channelId': 10,
-        'name': 'sampleChannel',
-        'isPublic' : false,
-        'allMembers' : [71],
-        'ownerMembers' : [1, 2, 3, 4, 5], //array of user Id's
-        'messages': [
-            {
-            'messageId' : 10,
-            'uId' : 90,
-            'timeSent' : 1332049834, //unix timestamp
-            'message' : 'Hello World'
-            },
-        ]
-    },
-  ],
+  users : [],  
+  channels : [],
 };
 
 // YOU SHOULDNT NEED TO MODIFY THE FUNCTIONS BELOW IN ITERATION 1
