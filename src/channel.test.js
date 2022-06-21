b9c776cb45816b469cc00a810bc801a338aeb25f
Ivan <z5421133@ad.unsw.edu.au>
2022-06-21 15:22:13 +1000

    Imported authRegisterV1 function from auth.js

--- src/channel.test.js
+++ src/channel.test.js
@@ -1,9 +1,17 @@
-import { channelDetailsV1, channelJoinV1, channelInviteV1, channelMessagesV1 } from './channel';
+import { 
+    channelDetailsV1, 
+    channelJoinV1, 
+    channelInviteV1, 
+    channelMessagesV1 
+} from './channel';
+
+import { authRegisterV1 } from './auth'; 
 import { clearV1 } from './other';
 
+
 beforeEach(() => {
-    const authUserId = authLogin('example123@email.com', 'password'); 
-    const channelId = channelsCreateV1(authUserId, 'John', true); 
+    const authUserId = authRegisterV1('example123@email.com', 'password', 'John', 'Smith'); 
+    const channelId = channelsCreateV1(authUserId, 'Channel 1', true); 
 });
 
 afterEach(() => {
@@ -11,25 +19,27 @@ afterEach(() => {
 }); 
 
 describe('Testing channelDetailsV1', () => {
-    test('channelId does not refer to valid channel', () => {
+    test('Case 1: channelId does not refer to valid channel', () => {
         const invalidId = Math.floor(Math.random() * 100);
         const details = channelDetailsV1(authUserId, invalidId); 
         expect(details).toStrictEqual({ error: 'error' });  
     });  
 
-    test('authorised user is not a member of channel', () => {
+    test('Case 2: authorised user is not a member of channel', () => {
         const invalidId = Math.floor(Math.random() * 100);
         const details = channelDetailsV1(invalidId, channelId); 
         expect(details).toStrictEqual({ error: 'error' });  
     }); 
 
-    test('Deals with all valid arguments', () => {
+    test('Case 3: Deals with all valid arguments', () => {
         const details = channelDetailsV1(authUserId, channelId);
         expect(details).toStrictEqual({
-            name: 'John', 
+            name: 'Channel 1', 
             isPublic: true, 
             ownerMembers: [
-                
+                {
+                    
+                }
             ], 
             allMembers: [
 
