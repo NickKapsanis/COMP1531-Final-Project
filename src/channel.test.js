e998a838c83433d4b7503860e5fc14deffdff26d
Ivan <z5421133@ad.unsw.edu.au>
2022-06-21 22:59:58 +1000

    Fixed test file by placing beforeEach into describe and imported channelsListallV1 for finding invalidIds

--- src/channel.test.js
+++ src/channel.test.js
@@ -6,29 +6,48 @@ import {
 } from './channel';
 
 import { authRegisterV1 } from './auth'; 
-import { channelsCreateV1 } from '.channels'
-import { clearV1 } from './other';
-
-beforeEach(() => {
-    const authUserId = authRegisterV1('example123@email.com', 'password', 'John', 'Smith'); 
-    const channelId = channelsCreateV1(authUserId, 'Channel 1', true); 
-});
-
-afterEach(() => {
-    clearV1();
-}); 
+import { channelsCreateV1, channelsListV1, channelsListallV1 } from '.channels'
+import { clearV1 } from './other'; 
 
 describe('Testing channelMessagesV1', () => {
+    let authUserId; 
+    let channelId; 
+
+    beforeEach(() => {
+        authUserId = authRegisterV1('example123@email.com', 'password', 'John', 'Smith'); 
+        channelId = channelsCreateV1(authUserId, 'Channel 1', true); 
+    });
+    
+    afterEach(() => {
+        clearV1();
+    });
+
     test('Case 1: channelId does not refer to valid channel', () => {
-        const invalidId = Math.floor(Math.random() * 100);
+        // Finding an invalid channelId to pass in 
+        const allChannels = channelsListallV1(authUserId);
+        let invalidId = 199; 
+        for (const i in allChannels) {
+            if (notMemberId === allChannels[i].channelId) {
+                notMemberId = notMemberId + 100; 
+            }
+        }
+
         const start = 0; 
         const messages = channelMessagesV1(authUserId, invalidId, start); 
         expect(messages).toStrictEqual({ error: 'error' });  
     });  
 
     test('Case 2: authorised user is not a member of channel', () => {
+        const memberOf = channelsListV1(authUserId); 
+        let notMemberId = 199; 
+        for (const i in memberOf) {
+            if (notMemberId === memberOf[i].channelId) {
+                notMemberId = notMemberId + 100; 
+            }
+        }
+
         const start = 0; 
-        const messages = channelMessagesV1(authUserId, channelId, start); 
+        const messages = channelMessagesV1(authUserId, notMemberId, start); 
         expect(messages).toStrictEqual({ error: 'error' });  
     }); 
 
