48cede6fe73d088ba55f8d55440124e3051a902c
Nick Kapsanis <z5254990@ad.unsw.edu.au>
2022-06-21 22:33:37 +1000

    implemented clearV1 to reset the datastore in dataStore.js and clear all users and channels

--- assumptions.md
+++ assumptions.md
@@ -1,2 +1,1 @@
 
-the helper function containsEmail, called in authRegisterV1 in auth.js assumes that the data contains an array called 'users'
--- src/other.js
+++ src/other.js
@@ -1,5 +1,13 @@
 import {getData, setData} from './dataStore';
+/*
+clearV1 resets the data from dataStore.js to be empty as according to the structure in data.md
 
+Arguments:
+    VOID
+
+Return Value:
+    VOID
+*/
 function clearV1() {
 let data = getData();
 data = {
