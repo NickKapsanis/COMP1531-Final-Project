dafa6f7f782e12ff45248f1c8d2195293ce08491
Nick Kapsanis <z5254990@ad.unsw.edu.au>
2022-06-23 12:54:55 +1000

    updated testing of clearV1 in other.test.js to use currently implemented function in master, and pulled current auth.js from master to complete said tests

--- src/other.test.js
+++ src/other.test.js
@@ -1,2 +1,18 @@
-import {clearV1} from './other'
+import { authRegisterV1, authLoginV1 } from './auth.js'
+import { clearV1 } from './other.js'
+
+////////////////////////////////////////////////
+/////          Tests for clearV1()         /////
+////////////////////////////////////////////////
+
+describe('Testing clear()', () => {
+
+  test('', () => {
+    
+    const testAuthId = authRegisterV1('testemail@email.com', 'testPassword123', 'testFirstName', 'testLastName');
+    clearV1();
+    expect(authLoginV1('testemail@email.com', 'testPassword123')).toStrictEqual({ error : 'error' });
+  });
+});
+
 
