381fbfa2e5495b9cfb6c0f75bc9a12095eec1961
Ansh Deo Singh <anshdeosingh@gmail.com>
2022-06-23 15:08:28 +1000

    Added getUId function and fixed bug in testing

--- src/other.test.js
+++ src/other.test.js
@@ -1,5 +1,5 @@
 import { authRegisterV1, authLoginV1 } from './auth.js'
-import { clearV1 } from './other.js'
+import { clearV1, getUId } from './other.js'
 
 ////////////////////////////////////////////////
 /////          Tests for clearV1()         /////
