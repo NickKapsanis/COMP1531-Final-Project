b8ed7d5aa82952c21e3a57b3a8c3171fbb074e59
Ivan <z5421133@ad.unsw.edu.au>
2022-06-23 20:34:52 +1000

    Modified test case 1 to not generate random numbers

--- src/channel.test.js
+++ src/channel.test.js
@@ -6,7 +6,7 @@ import {
 } from './channel';
 
 import { authRegisterV1 } from './auth'; 
-import { channelsCreateV1, channelsListV1 } from './channels'; 
+import { channelsCreateV1, channelsListV1, channelsListallV1 } from './channels'; 
 import { clearV1 } from './other';
 
 describe('Testing channelDetailsV1', () => {
@@ -23,14 +23,22 @@ describe('Testing channelDetailsV1', () => {
     }); 
 
     test('Case 1: channelId does not refer to valid channel', () => { 
-        const invalidId = Math.floor(Math.random() * 100);
+        // Finding an invalid channelId to pass in 
+        const allChannels = channelsListallV1(authUserId);
+        let invalidId = 199;  
+        for (const i in allChannels) {
+            if (invalidId === allChannels[i].channelId) {
+                invalidId = invalidId + 100; 
+            }
+        }
+
         const details = channelDetailsV1(authUserId, invalidId); 
         expect(details).toStrictEqual({ error: 'error' });  
     });  
 
     test('Case 2: authorised user is not a member of channel', () => {
         const memberOf = channelsListV1(authUserId); 
-        let notMemberId = 100; 
+        let notMemberId = 199; 
         for (const i in memberOf) {
             if (notMemberId === memberOf[i].channelId) {
                 notMemberId = notMemberId + 100; 
