b26d0974279708c9d2af2226841c5a9ec397a4ff
Ansh Deo Singh <anshdeosingh@gmail.com>
2022-06-23 15:56:52 +1000

    Files that are irrelevant to userProfileV1() aligned with master

--- src/channels.js
+++ src/channels.js
@@ -1,4 +1,3 @@
-import {getData, setData} from './dataStore'
 
 // Stub for channelsListV1 function
 function channelsListV1(authUserId) {
@@ -12,34 +11,5 @@ function channelsListallV1(authUserId) {
 
 // Stub for channelsCreateV1 function
 function channelsCreateV1(authUserId, name, isPublic) {
-    let data = getData();
-    let creator = data.users.find(i => i.authUserId === authUserId);
-    
-    // Error cases
-    if (creator === undefined) { return { error : 'error' } };
-    if (name.length > 20 || name.length < 1) { return { error : 'error' } };
-
-    const newChannelId = data.channels.length + 1;
-
-    let newChannel = {
-        
-        'channelId': newChannelId,
-        'name': name,
-        'isPublic' : isPublic,
-        'allMembers' : [creator.uId],
-        'ownerMembers' : [creator.uId], 
-        'messages': []
-    }
-
-    data.channels.push(newChannel);
-    data.users = data.users.filter(i => i.authUserId != authUserId);
-    creator.channels.push(newChannelId);
-    data.users.push(creator);
-
-    setData(data);
-
-    return newChannelId;
-    
+    return 'authUserId' + 'name' + 'isPublic';
 }
-
-export {channelsListV1, channelsListallV1, channelsCreateV1}
