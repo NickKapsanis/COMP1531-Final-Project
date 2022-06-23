5e51320d7c2bea4a270483b68c5c101a199fb9f9
z5413887 <z5413887@cse.unsw.edu.au>
2022-06-23 15:31:13 +1000

    implementing channelInviteV1

--- src/channel.js
+++ src/channel.js
@@ -36,11 +36,12 @@ function channelJoinV1(authUserId, channelId) {
 
     // error when the channel is private, and the user is not already a member or a global owner
     if (!channel.isPublic) {
-        if (!user.isGlobalOwner) {
+        if (!user.isGlobalOwner === 1) {
             return {error:'error'};
         }
     }
 
+    // update dataStore.js
     data.users.authUserId.channels.push(channel);
     data.channels.channelId.allMembers.push(user);
     setData(data);
@@ -48,6 +49,9 @@ function channelJoinV1(authUserId, channelId) {
 }
 
 
+//Invites a user with ID uId to join a channel with ID channelId. 
+//Once invited, the user is added to the channel immediately. 
+//In both public and private channels, all members are able to invite users.
 function channelInviteV1(authUserId, channelId, uId) {
     let data = getData();
     // setup to find the correct user and channel object
@@ -56,14 +60,36 @@ function channelInviteV1(authUserId, channelId, uId) {
     let userInviting, channel, userJoining;
     for (let users of usersArray) {
         if (authUserId === users.authUserId) {
-            userInviting = users;}}
+            userInviting = users;}
+        if (uId === users.uId) {
+            userJoining = users;}}
     for (let channels of channelsArray) {
         if (channelId === channels.channelId) {
             user = users;}}
-
-
-    //Invites a user with ID uId to join a channel with ID channelId. 
-    //Once invited, the user is added to the channel immediately. 
-    //In both public and private channels, all members are able to invite users.
     
+    // error when any of channelId does not refer to a valid channel
+    if (! channel.channelId === channelId) {return {error:'error'}};
+
+    // error when AuthuserId or uId does not refer to a valid user
+    if (! userInviting.authUserId === authUserId) {return {error:'error'}};
+    if (! userJoining.uId === uId) {return {error:'error'}};
+
+    // error when uId refers to user already in channel
+    for (let joinedChannels of userJoining.channels) {
+        if (channel === joinedChannels) {return {error:'error'}};
+    }
+
+    // error when authUserId is not a member of the channel
+    let seen = false;
+    for (let joinedChannels of userInviting.channels) {
+        if (joinedChannels.channelId === channelId) {
+            seen = true;
+        }
+    } if (!seen) {return {error:'error'}};
+    
+    // updating dataStore.js
+    data.users.uId.channels.push(channel);
+    data.channels.channelId.allMembers.push(userJoining);
+    setData(data);
+    return {};
 }
