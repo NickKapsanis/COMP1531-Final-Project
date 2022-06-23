23238ef6c64206aa82885c4dfd996c21739295d0
Manav <z5417834@unsw.edu.au>
2022-06-23 16:01:27 +1000

    fixed some minor bugs, added proper documentation

--- src/channels.js
+++ src/channels.js
@@ -1,16 +1,24 @@
 import {getData, setData} from './dataStore';
 import {getUId} from './other';
 
-//this function gives an array of all channels the user is in
+/*
+this function gives an array of all channels the user is in
+
+Arguments:
+    authUserId: integer    - the users unique identification number
+
+Return Value:
+    channelsArray: array    - an array of all channels user is in.
+*/
 export function channelsListV1(authUserId) {
     const data = getData();
     let uId = getUId(authUserId);
-    var numChannels = 0;
+    let numChannels = 0;
     let channelsArray = [];
 
     //this loop searches for given user id within every channel.   
-    for (var i = 0; i < data.channels.length; i++) {
-        for (var n = 0; n < data.channels[i].allMembers.length; n++) {
+    for (let i = 0; i < data.channels.length; i++) {
+        for (let n = 0; n < data.channels[i].allMembers.length; n++) {
             if (data.channels[i].allMembers[n] === uId) {
                 numChannels++;
 
@@ -31,20 +39,30 @@ export function channelsListV1(authUserId) {
     return channelsArray;
 }
 
-//this function gives an array of all channels
+
+
+/*
+this function gives an array of all channels
+
+Arguments:
+    authUserId: integer    - the users unique identification number
+
+Return Value:
+    allChannelsArray: array    - an array of all channels.
+*/
 export function channelsListallV1(authUserId) {
     const data = getData();
     let uId = getUId(authUserId);
-    var numChannels = 0;
-    var allChannelsArray = [];
+    let numChannels = 0;
+    let allChannelsArray = [];
 
     //this loop finds all arrays, adds them to channelsArray  
-    for (var j = 0; j < data.channels.length; j++) {
+    for (let j = 0; j < data.channels.length; j++) {
         numChannels++;
 
         let channel = {
-            channelId: data.channels[i].channelId,
-            name: data.channels[i].name
+            channelId: data.channels[j].channelId,
+            name: data.channels[j].name
         }
         allChannelsArray.push(channel);
     }
