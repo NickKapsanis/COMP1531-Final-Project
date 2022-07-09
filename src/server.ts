b78327822f5c05237729ab5e274b704280e988af
Nick Kapsanis <z5254990@ad.unsw.edu.au>
2022-07-09 10:50:34 +1000

    added http call of auth/register/v2 in server.ts not final implementation

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,9 @@ import express from 'express';
 import { echo } from './echo';
 import morgan from 'morgan';
 import config from './config.json';
+import { authRegisterV1 } from './auth';
 
+export { PORT, HOST };
 // Set up web app, use JSON
 const app = express();
 app.use(express.json());
@@ -14,7 +16,20 @@ const HOST: string = process.env.IP || 'localhost';
 app.get('/echo', (req, res, next) => {
   try {
     const data = req.query.echo as string;
+ //   res.send(JSON.stringify({
+ //     url: 'http://' + HOST + ':' + PORT,
+//    }))
     return res.json(echo(data));
+  } catch (err) {
+    next(err);
+  }
+});
+
+// authRegister
+app.post('/auth/register/v2', (req, res, next) => {
+  try {
+    const data = req.body;
+    return authRegisterV1(data.email, data.password, data.namefirst, data.namelast); 
   } catch (err) {
     next(err);
   }
