42b1a96cfae2a9278d13e58976cec80b53564975
Nick Kapsanis <z5254990@ad.unsw.edu.au>
2022-07-09 11:19:40 +1000

    wrapped testing for authLoginV1 in HTTP calls

--- src/auth.test.ts
+++ src/auth.test.ts
@@ -70,35 +70,63 @@ describe('testing registration for sucess', () => {
   });
 });
 
-// describe('testing authLoginV1 for input errors', () => {
+ describe('testing authLoginV1 for input errors', () => {
 
-//   let authUserId1;
-//   beforeEach( () => {
-//     clearV1();
-//     authUserId1 = authRegisterV1('TheEmail@gmail.com', '1234567', 'James', 'Bond').authUserId;
-//   });
+   beforeEach( () => {
+     const info = {
+      email: 'TheEmail@gmail.com',
+       password: '1234567',
+  }
+  const res = request(
+      'POST', 
+      url + '/auth/register/v2', 
+      {
+        body: JSON.stringify({
+          email: info.email,
+          password: info.password,
+        })
+      })  
+   });
 
-//   test.each([
-//     {email: 'NotTheEmail@gmail.com', password: '1234567', d: 'incorrect email (email does not belong to a user)'},
-//     {email: 'TheEmail@gmail.com', password: 'notThePassword', d: 'incorrect password (password does not match the email given)'},
-//     {email: 'NotTheEmail@gmail.com', password: 'notThePassword', d: 'incorrect email and password'},
-//    // {email: '', password: '', d: ''},
-//   ]) ('$d', ({email, password, d}) => {
-//     expect(authLoginV1(email, password)).toEqual({error: 'error'});
-//   });
+  test.each([
+    {email: 'NotTheEmail@gmail.com', password: '1234567', d: 'incorrect email (email does not belong to a user)'},
+    {email: 'TheEmail@gmail.com', password: 'notThePassword', d: 'incorrect password (password does not match the email given)'},
+    {email: 'NotTheEmail@gmail.com', password: 'notThePassword', d: 'incorrect email and password'},
+  ]) ('$d', ({email, password, d}) => {
+    const res = request(
+      'POST', 
+      url + '/auth/login/v2', 
+      {
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        })
+      })
+        const bodyObj = JSON.parse(String(res.getBody()));
+        expect(bodyObj).toEqual({error: 'error'});
+        expect(res.statusCode).toStrictEqual(200);
+   });
 
-//   test.each([
-//     {email: 'TheEmail@gmail.com', password: '1234567', d: 'email and password match'},
-//    // {email: '', password: '', d: ''},
-//   ]) ('$d', ({email, password, d}) => {
-//     expect(authLoginV1(email, password).authUserId).toEqual(authUserId1);
-//   });
-
-//   test.each([
-//     {email: '', password: '', d: 'Both string empty'},
-//     {email: 12, password: '123', d: 'email is not a string'},
-//     {email: 'email@email.com', password: 12, d: 'password is not a string'},
-//   ]) ('$d', ({email, password, d}) => {
-//     expect(authLoginV1(email, password)).toEqual({error : 'error'});
-//   })
-// });
+  test.each([
+    {email: 'TheEmail@gmail.com', password: '1234567', d: 'email and password match'},
+   // {email: '', password: '', d: ''},
+  ]) ('$d', ({email, password, d}) => {
+    const res = request(
+      'POST', 
+      url + '/auth/login/v2', 
+      {
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        })
+      })
+    const bodyObj = JSON.parse(String(res.getBody()));
+    expect(bodyObj).toEqual(
+      expect.objectContaining({
+        token: expect.any(Number),
+        authUserId: expect.any(Number),
+      })
+    );
+    expect(res.statusCode).toStrictEqual(200);
+  });
+ });
