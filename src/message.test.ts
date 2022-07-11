08b62bc67afac9cbe3f309400aeb80fdc2e87f66
Ivan <z5421133@ad.unsw.edu.au>
2022-07-11 21:48:35 +1000

    Written tests for message/send/v1

---
+++ src/message.test.ts
@@ -0,0 +1,161 @@
+import request from 'sync-request';
+import config from './config.json';
+
+const OK = 200;
+const port = config.port;
+const url = config.url;
+
+describe('Tests for message/send/V1', () => {
+    let token1; 
+    let token2;  
+    let channelId1;
+    let channelId2; 
+
+    beforeEach(() => {
+        //  Channels token[x] is member of: token1: [1], token2: [2]
+        token1 = requestAuthUserRegisterV2('example1@email.com', 'password1', 'John', 'Smith'); 
+        token2 = requestAuthUserRegisterV2('example2@email.com', 'password2', 'Jane', 'Citizen'); 
+        channelId1 = requestChannelsCreateV2(token1, 'Channel 1', true); 
+        channelId2 = requestChannelsCreateV2(token2, 'Channel 2', true);
+    }); 
+
+    afterEach(() => {
+        requestClear(); 
+    }); 
+
+    test('Case 1: token is invalid', () => {
+        const res = testRequestMessageSendV1('invalid-token', channelId1, 'Message 1'); 
+
+        const bodyObj = JSON.parse(String(res.getBody())); 
+        expect(res.statusCode).toBe(OK);
+        expect(bodyObj).toStrictEqual({ error: 'error' }); 
+    }); 
+
+    test('Case 2: channelId does not refer to valid channel', () => {
+        // Get all channels token is a part of 
+        const allChannels = requestChannelsListallV2(token1); 
+        let invalidId = 199;  
+        for (const i in allChannels) {
+            if (invalidId === allChannels[i].channelId) {
+                invalidId = invalidId + 100; 
+            }
+        }
+
+        const res = testRequestMessageSendV1(token1, invalidId, 'Message 1'); 
+
+        const bodyObj = JSON.parse(String(res.getBody())); 
+        expect(res.statusCode).toBe(OK);
+        expect(bodyObj).toStrictEqual({ error: 'error' }); 
+    }); 
+
+    test('Case 3: length of message is less than 1 or more than 1000 characters', () => {
+        // Generate 1000+ character message 
+        const testMessage100 = 'dlXqa8qv6YSWfOcAO7Vf9gAjigjRXGjHygJahreDg0yKUIpKRKhQWpruNwESu7nKdwtJU0zGsM34tgCm9CaWyPkV4hhVClmFfQNM'
+        let testMessage1000 = ''; 
+        for (let i = 0; i < 11; i++) {
+            testMessage1000 = testMessage1000 + testMessage100;  
+        }
+
+        const res1 = testRequestMessageSendV1(token1, channelId1, ''); 
+        const res2 = testRequestMessageSendV1(token1, channelId1, testMessage1000); 
+
+        expect(res1.statusCode).toBe(OK);
+        expect(res2.statusCode).toBe(OK);
+        const bodyObj1 = JSON.parse(String(res1.getBody())); 
+        const bodyObj2 = JSON.parse(String(res2.getBody())); 
+
+        expect(bodyObj1).toStrictEqual({ error: 'error' }); 
+        expect(bodyObj2).toStrictEqual({ error: 'error' }); 
+    }); 
+
+    test('Case 4: user not member of channel', () => {
+        const res = testRequestMessageSendV1(token1, channelId2, 'Message 1'); 
+        
+        const bodyObj = JSON.parse(String(res.getBody())); 
+        expect(res.statusCode).toBe(OK);
+        expect(bodyObj).toStrictEqual({ error: 'error' }); 
+    }); 
+
+    test('Case 5: successful message send', () => {
+        const res = testRequestMessageSendV1(token1, channelId1, 'Message 1'); 
+        
+        const bodyObj = JSON.parse(String(res.getBody())).messageId; 
+        expect(res.statusCode).toBe(OK);
+        expect(bodyObj).toStrictEqual(expect.any(Number)); // TODO: check if working 
+    }); 
+});
+
+function testRequestMessageSendV1(token: string, channelId: number, message: string) {
+    return request(
+        'POST', 
+        `${url}:${port}/message/send/v1`,
+        {
+            json: {
+                token: token, 
+                channelId: channelId, 
+                message: message, 
+            }
+        }
+    ); 
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+////////////////////        Helper Functions       ///////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+function requestAuthUserRegisterV2(email: string, password: string, nameFirst: string, nameLast: string) {
+    const res = request(
+        'POST', 
+        `${url}:${port}/auth/register/v2`, 
+        {
+            json: {
+                email: email, 
+                password: password, 
+                nameFirst: nameFirst, 
+                nameLast: nameLast
+            }
+        }
+    ); 
+
+    return JSON.parse(String(res.getBody())).token; 
+}
+
+function requestChannelsCreateV2(token: string, name: string, isPublic: boolean) {
+    const res = request(
+        'POST', 
+        `${url}:${port}/channels/create/v2`, 
+        {
+            json: {
+                token: token, 
+                name: name, 
+                isPublic: isPublic, 
+            }
+        }
+    ); 
+
+    return JSON.parse(String(res.getBody())).channelId; 
+}
+
+function requestChannelsListallV2(token: string) {
+    const res = request(
+        'GET', 
+        `${url}:${port}/channels/listall/v2`, 
+        {
+            qs: {
+                token: token, 
+            }
+        }
+    ); 
+
+    return JSON.parse(String(res.getBody())).channels; 
+}
+
+function requestClear() {
+    const res = request(
+        'DELETE',
+        `${url}:${port}/clear/v1`
+    );
+  
+    return JSON.parse(String(res.getBody()));
+}
