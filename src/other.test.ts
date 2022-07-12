9122b0b2fa4bd0c6a888662cf1bd10c9ad8a188d
Ansh Deo Singh <anshdeosingh@gmail.com>
2022-07-12 13:44:08 +1000

    Fixed linting errors

--- src/other.test.ts
+++ src/other.test.ts
@@ -9,30 +9,25 @@ const url = config.url;
 ///          Tests for clearV1()         /////
 //////////////////////////////////////////////
 
-
 describe('Testing clearV1()', () => {
-
   test('', () => {
-
-    const res1 = request(
+    request(
       'POST',
       `${url}:${port}/auth/register/v2`,
       {
         json: {
-          email: "testemail@email.com",
-          password: "testPassword123",
-          nameFirst: "testFirstName",
-          nameLast: "testLastName"
+          email: 'testemail@email.com',
+          password: 'testPassword123',
+          nameFirst: 'testFirstName',
+          nameLast: 'testLastName'
         }
       }
     );
 
-    const testAuthId = JSON.parse(String(res1.getBody())).authUserId;
-
     const res2 = request(
       'DELETE',
       `${url}:${port}/clear/v1`
-    )
+    );
 
     const body = JSON.parse(String(res2.getBody()));
     expect(res2.statusCode).toBe(OK);
@@ -43,8 +38,8 @@ describe('Testing clearV1()', () => {
       `${url}:${port}/auth/login/v2`,
       {
         json: {
-          email: "testemail@email.com",
-          password: "testPassword123",
+          email: 'testemail@email.com',
+          password: 'testPassword123',
         }
       }
     );
@@ -52,7 +47,5 @@ describe('Testing clearV1()', () => {
     const body1 = JSON.parse(String(res3.getBody()));
     expect(res3.statusCode).toBe(OK);
     expect(body1).toStrictEqual({ error: 'error' });
-
   });
 });
-
