2ed8a6c3733531eeab8dfd67e5543c514bbb4168
Ansh Deo Singh <anshdeosingh@gmail.com>
2022-07-13 02:59:17 +1000

    added server functionality for dm/remove dm/create dm/list

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import { echo } from './echo';
 import morgan from 'morgan';
 import config from './config.json';
 
+import { dmCreateV1, dmListV1, dmRemoveV1 } from './dm'
+
 // Set up web app, use JSON
 const app = express();
 app.use(express.json());
@@ -18,6 +20,27 @@ app.get('/echo', (req, res, next) => {
   } catch (err) {
     next(err);
   }
+});
+
+app.post('dm/create/v1', (req, res) => {
+
+  const {token, uIds} = req.body;
+  res.json(dmCreateV1(token, uIds));
+
+});
+
+app.get('dm/list/v1', (req, res) => {
+
+  const token = req.query;
+  res.json(dmListV1(token));
+
+});
+
+app.delete('dm/remove/v1', (req, res) => {
+
+  const {token, dmId} = req.query;
+  res.json(dmRemoveV1(token, dmId));
+
 });
 
 // for logging errors
