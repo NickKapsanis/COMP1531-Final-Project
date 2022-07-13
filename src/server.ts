6bd7aa27564b1199174a940ff7eeac652ddba4cb
Ansh Deo Singh <anshdeosingh@gmail.com>
2022-07-14 02:31:15 +1000

    all auth functions brought in

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,14 @@ import express from 'express';
 import { echo } from './echo';
 import morgan from 'morgan';
 import config from './config.json';
-import { authRegisterV1, authLoginV1, authLogoutV1 } from './auth';
+import { clearV1 } from './other';
 
 // Set up web app, use JSON
 const app = express();
 app.use(express.json());
 
-const PORT: number = parseInt(process.env.PORT || config.port);
-const HOST: string = process.env.IP || 'localhost';
-export { PORT, HOST };
+export const PORT: number = parseInt(process.env.PORT || config.port);
+export const HOST: string = process.env.IP || 'localhost';
 
 // Example get request
 app.get('/echo', (req, res, next) => {
@@ -22,20 +21,9 @@ app.get('/echo', (req, res, next) => {
   }
 });
 
-// authRegister
-app.post('/auth/register/v2', (req, res) => {
-  const data = req.body;
-  res.json(authRegisterV1(data.email, data.password, data.nameFirst, data.nameLast));
-});
-// authLogin
-app.post('/auth/login/v2', (req, res) => {
-  const data = req.body;
-  res.json(authLoginV1(data.email, data.password));
-});
-// authLogout
-app.post('/auth/logout/v1', (req, res) => {
-  const data = req.body;
-  res.json(authLogoutV1(data.token));
+// clearV1()
+app.delete('/clear/v1', (req, res) => {
+  res.json(clearV1());
 });
 
 // for logging errors
