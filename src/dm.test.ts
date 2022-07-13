c19c5326f64858233ab370bb72508226ede4de30
Nick Kapsanis <z5254990@ad.unsw.edu.au>
2022-07-13 15:05:37 +1000

    implemented testing for dm/messages/v1 in dm.test.ts

--- src/dm.test.ts
+++ src/dm.test.ts
@@ -272,6 +272,117 @@ describe('Testing dm/leave/v1', () => {
 // ///////////////////////////////////////////////////////////////////////////////////////////
 // ///////////////////////////////////////////////////////////////////////////////////////////
 // ///////////////////////////////////////////////////////////////////////////////////////////
+// Tests for dmMessagesV1
+describe('Testing dmMessagesV1', () => {
+
+    beforeEach(() => {
+    });
+  
+    afterEach(() => {
+    });
+  
+    
+    test('dmId is not valid', () => {
+        /// ////////////////////////////set up the datastore/////////////////////////////////////////
+        const user1 = registerUser('testingUser1@gmail.com', '1234567', 'FirstName1', 'LastName1');
+        const badDmId = -100; // will work regardless as dm has not been created
+        /// /////////////////////////////////////////////////////////////////////////////////////////
+        const res = request(
+        'GET',
+        url + '/dm/messages/v1',
+        {
+            qs: {
+            token: user1.token,
+            dmId: badDmId,
+            start: 10,
+            }
+        }
+        );
+        const bodyObj = JSON.parse(res.body as string);
+        expect(res.statusCode).toBe(200);
+        expect(bodyObj).toEqual({ error: 'error' });
+    });
+    test('dmId is valid but authUserId is not a member', () => {
+        /// ////////////////////////////set up the datastore/////////////////////////////////////////
+        const user1 = registerUser('testingUser1@gmail.com', '1234567', 'FirstName1', 'LastName1');
+        const user2 = registerUser('testingUser2@gmail.com', '1234567', 'FirstName2', 'LastName2');
+        const user3 = registerUser('testingUser3@gmail.com', '1234567', 'FirstName3', 'LastName3');
+        const dm12 = startDm(user1.token, [giveUid(user2.authUserId)]);
+        /// /////////////////////////////////////////////////////////////////////////////////////////
+        const res = request(
+          'GET',
+          url + '/dm/messages/v1',
+          {
+            qs: {
+              token: user3.token,
+              dmId: dm12.dmId,
+              start: 10,
+            }
+          }
+        );
+        const bodyObj = JSON.parse(res.body as string);
+        expect(res.statusCode).toBe(200);
+        expect(bodyObj).toEqual({ error: 'error' });
+      });
+  
+    test('start is greater than total messages in channel', () => {
+    /// ////////////////////////////set up the datastore/////////////////////////////////////////
+    const user1 = registerUser('testingUser1@gmail.com', '1234567', 'FirstName1', 'LastName1');
+    const user2 = registerUser('testingUser2@gmail.com', '1234567', 'FirstName2', 'LastName2');
+    const dm12 = startDm(user1.token, [giveUid(user2.authUserId)]);
+    const start = 10 //there are no messages so any number > 0.
+    /// /////////////////////////////////////////////////////////////////////////////////////////
+    const res = request(
+        'GET',
+        url + '/dm/messages/v1',
+        {
+          qs: {
+            token: user1.token,
+            dmId: dm12.dmId,
+            start: start,
+          }
+        }
+      );
+      const bodyObj = JSON.parse(res.body as string);
+      expect(res.statusCode).toBe(200);
+      expect(bodyObj).toEqual({ error: 'error' });
+    });
+  
+    test('test for sucsessfull input', () => {
+    /// ////////////////////////////set up the datastore/////////////////////////////////////////
+    const user1 = registerUser('testingUser1@gmail.com', '1234567', 'FirstName1', 'LastName1');
+    const user2 = registerUser('testingUser2@gmail.com', '1234567', 'FirstName2', 'LastName2');
+    const dm12 = startDm(user1.token, [giveUid(user2.authUserId)]);
+    const start = 0 //there are no messages so 0 is the only usable size
+    /// /////////////////////////////////////////////////////////////////////////////////////////
+    const res = request(
+        'GET',
+        url + '/dm/messages/v1',
+        {
+          qs: {
+            token: user1.token,
+            dmId: dm12.dmId,
+            start: start,
+          }
+        }
+      );
+    const bodyObj = JSON.parse(String(res.getBody()));
+    expect(res.statusCode).toBe(200);
+    expect(bodyObj).toStrictEqual({
+        messages: [],
+        start: 0,
+        end: -1,
+        });
+    });
+  
+  });
+  
+// ///////////////////////////////////////////////////////////////////////////////////////////
+// ///////////////////////////////////////////////////////////////////////////////////////////
+// ///////////////////////////////////////////////////////////////////////////////////////////
+// ///////////////////////////////////////////////////////////////////////////////////////////
+// ///////////////////////////////////////////////////////////////////////////////////////////
+// ///////////////////////////////////////////////////////////////////////////////////////////
 // testing helper function, registers a user by making an http call. returns the body object of the response
 function registerUser(email: string, password: string, nameFirst: string, nameLast: string) {
   const res = request(
