f745a168ae6658c9e2017c7d686217d53fba3757
Ansh Deo Singh <anshdeosingh@gmail.com>
2022-07-14 02:48:31 +1000

    Added clear function and testing

---
+++ src/other.test.ts
@@ -0,0 +1,59 @@
+import request from 'sync-request';
+import { PORT, HOST } from './server';
+
+const OK = 200;
+const port = PORT;
+const url = HOST;
+
+/*
+/////////////////////////////////////////////
+///          Tests for clearV1()        /////
+/////////////////////////////////////////////
+*/
+
+describe('Testing clearV1()', () => {
+  test('', () => {
+    request(
+      'POST',
+      `${url}:${port}/auth/register/v2`,
+      {
+        body: JSON.stringify({
+          email: 'testemail@email.com',
+          password: 'testPassword123',
+          nameFirst: 'testFirstName',
+          nameLast: 'testLastName'
+        }),
+        headers: {
+          'Content-type': 'application/json',
+        }
+      }
+    );
+
+    const res2 = request(
+      'DELETE',
+      `${url}:${port}/clear/v1`
+    );
+
+    const body = JSON.parse(String(res2.getBody()));
+    expect(res2.statusCode).toBe(OK);
+    expect(body).toStrictEqual({});
+
+    const res3 = request(
+      'POST',
+      `${url}:${port}/auth/login/v2`,
+      {
+        body: JSON.stringify({
+          email: 'testemail@email.com',
+          password: 'testPassword123',
+        }),
+        headers: {
+          'Content-type': 'application/json',
+        }
+      }
+    );
+
+    const body1 = JSON.parse(String(res3.getBody()));
+    expect(res3.statusCode).toBe(OK);
+    expect(body1).toStrictEqual({ error: 'error' });
+  });
+});
---
+++ src/other.ts
@@ -0,0 +1,51 @@
+import { getData, setData, dataStoreType } from './dataStore';
+
+type errorMessage = {
+  error: 'error'
+}
+
+type uId = number
+
+// type empty = Record<string, never>
+
+/* Given an authUId returns the uId of the corresponding user
+Parameters:
+    authUserId - (integer)
+
+Return Value:
+    (1) Error returned if authUserId does not exist
+    {error : 'error'}
+
+    (2) In case of no error
+    uId - (integer)
+*/
+function getUId(authUserId : number) : errorMessage | uId { // Does this need a token?
+  const data = getData();
+  const user = data.users.find(i => i.authUserId === authUserId);
+  if (user === undefined) {
+    return { error: 'error' };
+  }
+  return user.uId;
+}
+
+/*
+clearV1 resets the data from dataStore.js to be empty as according to the structure in data.md
+
+Arguments:
+    VOID
+
+Return Value:
+    VOID
+*/
+function clearV1() {
+  const newData : dataStoreType = {
+    users: [],
+    channels: [],
+    dms: [],
+  };
+
+  setData(newData);
+  return {};
+}
+
+export { clearV1, getUId };
