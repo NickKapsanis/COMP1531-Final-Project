a5bc6a497d6dadc7f2389a8ee56e15a33122c5d8
Nick Kapsanis <z5254990@ad.unsw.edu.au>
2022-07-13 13:42:18 +1000

    implemented /dm/leave/v1 in server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { echo } from './echo';
 import morgan from 'morgan';
 import config from './config.json';
-import { dmDetailsV1 } from './dm';
+import { dmDetailsV1, dmLeaveV1 } from './dm';
 
 // Set up web app, use JSON
 const app = express();
@@ -27,6 +27,11 @@ app.get('/dm/details/v1', (req, res) => {
   const token = req.query.token as string;
   const dmId = parseInt(req.query.dmId as string);
   res.json(dmDetailsV1(token, dmId));
+});
+// request dm leave V1
+app.post('/dm/leave/v1', (req, res) => {
+  const data = req.body;
+  res.json(dmLeaveV1(data.token, data.dmId));
 });
 
 // for logging errors
