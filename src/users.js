9c66cec6b63c758f40e781be04776c3c7e1802be
Manav Dodia <manav@Manavs-MacBook-Air.local>
2022-07-14 21:01:11 +1000

    resolving merge conflicts with master

---
+++ src/users.js
@@ -0,0 +1,40 @@
+import {getData} from './dataStore'
+/*
+Given an authUserId and a uId, the function userProfileV1
+returns the user information of the corresponding uId
+
+* Parameters :
+    authUserId (integer)
+    uId        (integer)
+
+* Return values :
+  (1) Error returned if either authUserId or uId do not exist
+    {error : 'error'} 
+   
+  (2) User information returned for user with uId given as parameter
+    { 
+      uId       : (integer)   
+      email     : (string)
+      nameFirst : (string)
+      nameLast  : (string)
+      handleStr : (string)
+    }
+*/
+
+function userProfileV1(authUserId, uId) {
+  let data = getData();
+  let user1 = data.users.find(i => i.authUserId === authUserId);
+  let user2 = data.users.find(i => i.uId === uId);
+
+  if (user1 === undefined || user2 === undefined) {return { error : 'error'} };
+  
+  return {
+    uId: user2.uId, 
+    email: user2.email,
+    nameFirst: user2.nameFirst, 
+    nameLast: user2.nameLast, 
+    handleStr: user2.handleStr
+  }
+}
+
+export { userProfileV1 }
--- src/users.test.js
+++ src/users.test.js
@@ -1,25 +1,18 @@
-/*
-import { createChannel, createUser, channelJoin } from './channel.test';
-import { userSethandlelV1 } from './users';
-import { getUId } from './other';
+import { authRegisterV1 } from './auth.js'
+import { clearV1, getUId } from './other.js'
+import { userProfileV1 } from './users.js'
 
-import request from 'sync-request';
-import { PORT, HOST } from './server';
+////////////////////////////////////////////////
+/////      Tests for userProfileV1() 	     /////
+////////////////////////////////////////////////
 
-const url = 'http://' + HOST + ':' + PORT;
-
-/// /////////////////////////////////////////////
-/// //      Tests for userProfileV1() 	     /////
-/// /////////////////////////////////////////////
-
-/*
 describe('Testing userProfileV1()', () => {
 
   test('Testing if error is returned if both authUserId and uId do not exist', () => {
 
     clearV1();
     expect(userProfileV1(-1,-3)).toStrictEqual({ error : 'error' });
-
+  
   });
 
   test('Testing if error is returned if authUserId does not exist', () => {
@@ -28,7 +21,7 @@ describe('Testing userProfileV1()', () => {
     const testAuthId = authRegisterV1('testemail@email.com', 'testPassword123', 'testFirstName', 'testLastName').authUserId;
     const testUId = getUId(testAuthId);
     expect(userProfileV1(-1, testUId)).toStrictEqual({ error : 'error' });
-
+  
   });
 
   test('Testing if error is returned if uId does not exist', () => {
@@ -36,7 +29,7 @@ describe('Testing userProfileV1()', () => {
     clearV1();
     const testAuthId = authRegisterV1('testemail@email.com', 'testPassword123', 'testFirstName', 'testLastName').authUserId;
     expect(userProfileV1(testAuthId, -1)).toStrictEqual({ error : 'error' });
-
+  
   });
 
   test('Testing correct output for when authUserId and uId belong to same person', () => {
@@ -67,49 +60,7 @@ describe('Testing userProfileV1()', () => {
   });
 
 });
-*/
 
-/// /////////////////////////////////////////////
-/// //      Tests for usersAllV1() 	         /////
-/// /////////////////////////////////////////////
-/*
-describe('Testing usersAllV1 - should all work if other functions work', () => {
-  let james, rufus, alex, rufusChannel, alexUId, rufusUId, jamesUId;
 
-  beforeEach(() => {
-    request('DELETE', url + '/clear/v1');
 
-    james = createUser('james@gmail.com', 'testPassword123', 'James', 'Brown');
-    jamesUId = getUId(james.authUserId);
-    userSethandlelV1(james.token, 'coolJames');
 
-    rufus = createUser('rufus@gmail.com', 'testPassword123', 'Rufus', 'Hayes');
-    rufusUId = getUId(rufus.authUserId);
-    userSethandlelV1(rufus.token, 'epicRufus');
-
-    alex = createUser('alex@gmail.com', 'testPassword123', 'Alex', 'King');
-    alexUId = getUId(alex.authUserId);
-    userSethandlelV1(alex.token, 'amazingAlex');
-
-    rufusChannel = createChannel(rufus.authUserId, 'testChannel2', true);
-    channelJoin(alex.token, rufusChannel);
-  });
-
-  test('test successful case', () => {
-    expect(usersAll(james.token)).toEqual([
-      { userId: jamesUId, email: 'james@gmail.com', nameFirst: 'James', nameLast: 'Brown', handleStr: 'coolJames' },
-      { userId: rufusUId, email: 'rufus@gmail.com', nameFirst: 'Rufus', nameLast: 'Hayes', handleStr: 'epicRufus' },
-      { userId: alexUId, email: 'alex@gmail.com', nameFirst: 'Alex', nameLast: 'King', handleStr: 'amazingAlex' },
-    ]);
-  });
-});
-
-const usersAll = (tokens: string) => {
-  const res = request(
-    'GET', url + '/users/all/v1',
-    { qs: { token: tokens } }
-  );
-  return JSON.parse(String(res.getBody()));
-};
-
-*/
