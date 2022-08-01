e63b3715590a922301985b9378d4024af1986115
Matt O'Dea <z5413887@ad.unsw.edu.au>
2022-08-01 21:52:24 +1000

    completed test suite for standup.ts

--- src/standup.test.ts
+++ src/standup.test.ts
@@ -1,4 +1,4 @@
-import { createChannel, createUser } from './channel.test';
+import { createChannel, createUser, channelJoin } from './channel.test';
 import request from 'sync-request';
 import config from './config.json';
 
@@ -12,87 +12,96 @@ const hosturl = config.url;
 const url = hosturl + ':' + port;
 
 // helper function - calls standupStart through the server
-const standupStart = (channelId: number, length: number) => {
-    const res = request(
-      'POST',
-      url + '/standup/start/v1',
-      {
-        body: JSON.stringify({ channelId: channelId, length: length }),
-        headers: {
-          'Content-type': 'application/json',
-        },
-      }
-    );
-    if (res.statusCode !== 200) {
-      return res.statusCode;
+const standupStart = (token: string, channelId: number, length: number) => {
+  const res = request(
+    'POST',
+    url + '/standup/start/v1',
+    {
+      body: JSON.stringify({ channelId: channelId, length: length }),
+      headers: {
+        token: token,
+        'Content-type': 'application/json',
+      },
+    }
+  );
+  if (res.statusCode !== 200) {
+    return res.statusCode;
   }
-    return JSON.parse(String(res.getBody()));
+  return JSON.parse(String(res.getBody()));
 };
 
 // helper function - calls standupActive through the server
-const standupActive = (channelId: number) => {
-    const res = request(
-        'GET',
-        url + '/standup/active/v1',
-        {
-          qs: {
-            channelId: channelId,
-          }
-        }
-    );
-    if (res.statusCode !== 200) {
-      return res.statusCode;
+const standupActive = (token: string, channelId: number) => {
+  const res = request(
+    'GET',
+    url + '/standup/active/v1',
+    {
+      qs: {
+        channelId: channelId,
+      },
+      headers: {
+        token: token,
+      }
+    }
+  );
+  if (res.statusCode !== 200) {
+    return res.statusCode;
   }
-return JSON.parse(String(res.getBody()));
+  return JSON.parse(String(res.getBody()));
 };
 
 // helper function - calls standupSend through the server
-const standupSend = (channelId: number, message: string) => {
-    const res = request(
-      'POST',
-      url + '/standup/send/v1',
-      {
-        body: JSON.stringify({ channelId: channelId, message: message }),
-        headers: {
-          'Content-type': 'application/json',
-        },
-      }
-    );
-    if (res.statusCode !== 200) {
-        return res.statusCode;
+const standupSend = (token: string, channelId: number, message: string) => {
+  const res = request(
+    'POST',
+    url + '/standup/send/v1',
+    {
+      body: JSON.stringify({ channelId: channelId, message: message }),
+      headers: {
+        token: token,
+        'Content-type': 'application/json',
+      },
     }
-    return JSON.parse(String(res.getBody()));
+  );
+  if (res.statusCode !== 200) {
+    return res.statusCode;
+  }
+  return JSON.parse(String(res.getBody()));
 };
-  
 
 /// /////////////////////////////////////////////
 /// //      Tests for standupStartV1()      /////
 /// /////////////////////////////////////////////
 describe('standupStart', () => {
-  let sampleChannel: number, John: userType;
+  let sampleChannel: { channelId: number }, John: userType;
 
   beforeEach(() => {
     request('DELETE', url + '/clear/v1');
     John = createUser('johnsmith@gmail.com', 'testPassword123', 'John', 'Smith');
     sampleChannel = createChannel(John.token, 'SampleChannel', true);
+    channelJoin(John.token, sampleChannel.channelId);
   });
 
   test('successful return value', () => {
-    let startTime: number = Math.floor((new Date()).getTime() / 1000)
-    expect(standupStart(sampleChannel, 10).toEqual(startTime+10));
+    const startTime: number = Math.floor((new Date()).getTime() / 1000);
+    expect(standupStart(John.token, sampleChannel.channelId, 10)).toEqual({ finishTime: startTime + 10 });
+  });
+  test('token is invalid', () => {
+    expect(standupStart('gobbledook', sampleChannel.channelId, 10)).toEqual(403);
   });
   test('bad channel Id', () => {
-    expect(standupStart(-100, 10).toEqual(400));
+    expect(standupStart(John.token, -100, 10)).toEqual(400);
   });
   test('length is a negative number', () => {
-    expect(standupStart(sampleChannel, -10).toEqual(400));
+    expect(standupStart(John.token, sampleChannel.channelId, -10)).toEqual(400);
   });
   test('standup is already active in channel', () => {
-    standupStart(sampleChannel, 10)
-    expect(standupStart(sampleChannel, 10).toEqual(400));
+    standupStart(John.token, sampleChannel.channelId, 10);
+    expect(standupStart(John.token, sampleChannel.channelId, 10)).toEqual(400);
   });
   test('user is not a member of the channel', () => {
-    //TODO - complete this
+    const Steve = createUser('steve@gmail.com', 'testPassword123', 'Steve', 'Smith');
+    expect(standupStart(Steve.token, sampleChannel.channelId, 10)).toEqual(403);
   });
 });
 
@@ -100,7 +109,7 @@ describe('standupStart', () => {
 /// //      Tests for standupActiveV1()     /////
 /// /////////////////////////////////////////////
 describe('standupActive', () => {
-  let sampleChannel: number, John: userType;
+  let sampleChannel: { channelId: number }, John: userType;
 
   beforeEach(() => {
     request('DELETE', url + '/clear/v1');
@@ -108,17 +117,19 @@ describe('standupActive', () => {
     sampleChannel = createChannel(John.token, 'SampleChannel', true);
   });
 
-  test('test', () => {
-    test('bad channel Id', () => {
-      expect(standupActive(-100).toEqual(400));
-    });
-    test('user is not a member of the channel', () => {
-      //TODO - complete this
-    });
-    test('success', () => {
-      let finishTime: number = standupStart(sampleChannel, 10);
-      expect(standupActive(sampleChannel).toEqual({ isActive: true , timeFinish: finishTime }));
-    });
+  test('bad channel Id', () => {
+    expect(standupActive(John.token, -100)).toEqual(400);
+  });
+  test('user is not a member of the channel', () => {
+    const Steve = createUser('steve@gmail.com', 'testPassword123', 'Steve', 'Smith');
+    expect(standupActive(Steve.token, sampleChannel.channelId)).toEqual(403);
+  });
+  test('success', () => {
+    const finishTime: { finishTime: number } = standupStart(John.token, sampleChannel.channelId, 10);
+    expect(standupActive(John.token, sampleChannel.channelId)).toEqual({ isActive: true, timeFinish: finishTime.finishTime });
+  });
+  test('token is invalid', () => {
+    expect(standupActive('gobbledook', sampleChannel.channelId)).toEqual(403);
   });
 });
 
@@ -126,7 +137,7 @@ describe('standupActive', () => {
 /// //       Tests for standupSendV1()      /////
 /// /////////////////////////////////////////////
 describe('standupSend', () => {
-  let sampleChannel: number, John: userType;
+  let sampleChannel: { channelId: number }, John: userType;
 
   beforeEach(() => {
     request('DELETE', url + '/clear/v1');
@@ -134,35 +145,38 @@ describe('standupSend', () => {
     sampleChannel = createChannel(John.token, 'SampleChannel', true);
   });
 
-  test('standupSend', () => {
-    test('success', () => {
-      standupStart(sampleChannel, 10);
-      expect(standupSend(sampleChannel, 'add this message to the queue').toEqual({}));
-    });
-    test('bad channel Id', () => {
-      standupStart(sampleChannel, 10);
-      expect(standupSend(-100, 'add this message to the queue').toEqual(400));
-    });
-    test('length of message exceeds 1000 chars (1050 chars)', () => {
-      standupStart(sampleChannel, 10);
-      expect(standupSend(sampleChannel, `bljUwrWuzjLodaeOV97GSZ23rlzLz6AyqSZm5V9EgJLxVv8ZIIyQI6HV
-      83X78XZaB53wCUVSUeuZpzOr9jpBEPxs4UR1qMZsX2DGR0P1GoRdugJ4F2Ct803XNlNMTFnQ6D5wwaokuKs
-      CQquJYLcdDkbFrWW9xZQCglXWzvpJgD3ITaNIddcnpusRhbhv1MsxLpNmUl5yTSAQaGuYDV2IJypWHu3H1XD
-      e6jqHnOsbXNskzMduAsNfpjl1aJTHsLjQnO77WIs0P9vjpho1ViZHqEC99wlRKdAbZjkxnD1Qdl2YVs1aoaAz
-      GEpkXF04whQe1fPg3iVrqU8QQXzi9awrt15bh5zb9osrgfmIhpMkMRaZwOOQRqTXLaXeEwCRrSvSrV8Ck5zw
-      HMg0qCRnMrpsytPw9j8i3JLHU8coVlANMSzjUw2qHh57gFRoxBci4fOmaS8OoLYXkl1Aa2hxMEB7rh22qrNy
-      UrldCcRjDrJxsahsRLFRsc0aijDFT0tiDMFgFfuHwTvuw4Notdpozf9lQHVjuHIEjbB6t9eqP2Q6EBBxBr8bm
-      7QzzKaLHbWsETyrRfQmjerJgTVcLpi9SxwQCHd1nqYjFchcBuVkwAiewsNFyAkSgSsOLbxxlf2SVV2XnCGPRL
-      ICkJotjrzoD6uqZ6E4kwBKcpaUDJ4vEMRvaDgcuxMRzVrWibbzvrPJDSoKLiCXiKPSNla2FW0Lh5NP1smOuSxp
-      p3we6nMpOjgu9PpevwCKfmRANMFa0lhOeKwOQTnAFsPOzPvZX3Jg07aZE0TkW4nD9Fkq0Jd0RrCelZS3OfEz
-      xbQkJ0qQ0iu2BAdvuRKpwOFhjldgLgDytciqT4s7xVl4lwVRWvrdiZqBdyMf3aqJx1KfNgOzBPTpLVOu7bZ6
-      tKP405NmY3lFhZrag6cXkdSAmEUibz4xNgJmPymwt5EF3geATfdYA8yHzz6K42wx`).toEqual(400));
-    });
-    test('no active standup', () => {
-      expect(standupSend(sampleChannel, 'add this message to the queue').toEqual(400));
-    });
-    test('user is not a member of the channel', () => {
-      //TODO - complete this
-    });
+  test('success', () => {
+    standupStart(John.token, sampleChannel.channelId, 10);
+    expect(standupSend(John.token, sampleChannel.channelId, 'add this message to the queue')).toEqual({});
+  });
+  test('bad channel Id', () => {
+    standupStart(John.token, sampleChannel.channelId, 10);
+    expect(standupSend(John.token, -100, 'add this message to the queue')).toEqual(400);
+  });
+  test('length of message exceeds 1000 chars (1050 chars)', () => {
+    standupStart(John.token, sampleChannel.channelId, 10);
+    expect(standupSend(John.token, sampleChannel.channelId, `bljUwrWuzjLodaeOV97GSZ23rlzLz6AyqSZm5V9EgJLxVv8ZIIyQI6HV
+    83X78XZaB53wCUVSUeuZpzOr9jpBEPxs4UR1qMZsX2DGR0P1GoRdugJ4F2Ct803XNlNMTFnQ6D5wwaokuKs
+    CQquJYLcdDkbFrWW9xZQCglXWzvpJgD3ITaNIddcnpusRhbhv1MsxLpNmUl5yTSAQaGuYDV2IJypWHu3H1XD
+    e6jqHnOsbXNskzMduAsNfpjl1aJTHsLjQnO77WIs0P9vjpho1ViZHqEC99wlRKdAbZjkxnD1Qdl2YVs1aoaAz
+    GEpkXF04whQe1fPg3iVrqU8QQXzi9awrt15bh5zb9osrgfmIhpMkMRaZwOOQRqTXLaXeEwCRrSvSrV8Ck5zw
+    HMg0qCRnMrpsytPw9j8i3JLHU8coVlANMSzjUw2qHh57gFRoxBci4fOmaS8OoLYXkl1Aa2hxMEB7rh22qrNy
+    UrldCcRjDrJxsahsRLFRsc0aijDFT0tiDMFgFfuHwTvuw4Notdpozf9lQHVjuHIEjbB6t9eqP2Q6EBBxBr8bm
+    7QzzKaLHbWsETyrRfQmjerJgTVcLpi9SxwQCHd1nqYjFchcBuVkwAiewsNFyAkSgSsOLbxxlf2SVV2XnCGPRL
+    ICkJotjrzoD6uqZ6E4kwBKcpaUDJ4vEMRvaDgcuxMRzVrWibbzvrPJDSoKLiCXiKPSNla2FW0Lh5NP1smOuSxp
+    p3we6nMpOjgu9PpevwCKfmRANMFa0lhOeKwOQTnAFsPOzPvZX3Jg07aZE0TkW4nD9Fkq0Jd0RrCelZS3OfEz
+    xbQkJ0qQ0iu2BAdvuRKpwOFhjldgLgDytciqT4s7xVl4lwVRWvrdiZqBdyMf3aqJx1KfNgOzBPTpLVOu7bZ6
+    tKP405NmY3lFhZrag6cXkdSAmEUibz4xNgJmPymwt5EF3geATfdYA8yHzz6K42wx`)).toEqual(400);
+  });
+  test('no active standup', () => {
+    expect(standupSend(John.token, sampleChannel.channelId, 'add this message to the queue')).toEqual(400);
+  });
+  test('user is not a member of the channel', () => {
+    const Steve = createUser('steve@gmail.com', 'testPassword123', 'Steve', 'Smith');
+    expect(standupSend(Steve.token, sampleChannel.channelId, 'message')).toEqual(403);
+  });
+  test('token is invalid', () => {
+    standupStart(John.token, sampleChannel.channelId, 10);
+    expect(standupSend('gobbledook', sampleChannel.channelId, 'message')).toEqual(403);
   });
 });
