3cec76b6bc0fab24b8dc1604ee786f1e6799f118
Ansh Deo Singh <anshdeosingh@gmail.com>
2022-08-01 15:03:57 +1000

    Added tests for users/stats and user/stats

--- src/server.ts
+++ src/server.ts
@@ -33,17 +33,20 @@ app.get('/echo', (req, res, next) => {
     next(err);
   }
 });
+
 // request dm details V1
 app.get('/dm/details/v1', (req, res) => {
   const token = req.query.token as string;
   const dmId = parseInt(req.query.dmId as string);
   res.json(dmDetailsV1(token, dmId));
 });
+
 // request dm leave V1
 app.post('/dm/leave/v1', (req, res) => {
   const data = req.body;
   res.json(dmLeaveV1(data.token, data.dmId));
 });
+
 // request dm messages V1
 app.get('/dm/messages/v1', (req, res) => {
   const token = req.query.token as string;
@@ -58,17 +61,21 @@ app.post('/dm/create/v1', (req, res) => {
   const uIds = req.body.uIds;
   res.json(dmCreateV1(token, uIds));
 });
+
 // dmList
 app.get('/dm/list/v1', (req, res) => {
   const token = String(req.query.token);
   res.json(dmListV1(token));
 });
+
 // dmRemove
 app.delete('/dm/remove/v1', (req, res) => {
   const token = String(req.query.token);
   const dmId = Number(req.query.dmId);
   res.json(dmRemoveV1(token, dmId));
 });
+
+// ChannelsCreate
 app.post('/channels/create/v2', (req, res) => {
   const token = String(req.body.token);
   const name = String(req.body.name);
@@ -115,25 +122,30 @@ app.post('/channel/join/v2', (req, res) => {
   const data = req.body;
   res.json(channelJoinV2(data.token, data.channelId));
 });
+
 // channelInvite
 app.post('/channel/invite/v2', (req, res) => {
   const data = req.body;
   res.json(channelInviteV2(data.token, data.channelId, data.uId));
 });
+
 // addChannelOwner
 app.post('/channel/addowner/v1', (req, res) => {
   const data = req.body;
   res.json(addChannelOwnerV1(data.token, data.channelId, data.uId));
 });
+
 // removeChannelOwner
 app.post('/channel/removeowner/v1', (req, res) => {
   const data = req.body;
   res.json(removeChannelOwnerV1(data.token, data.channelId, data.uId));
 });
+
 // clearV1
 app.delete('/clear/v1', (req, res) => {
   res.json(clearV1());
 });
+
 // usersAll
 app.get('/users/all/v1', (req, res) => {
   const token = req.query.token;
@@ -143,60 +155,41 @@ app.get('/users/all/v1', (req, res) => {
   }
   res.send(JSON.stringify(usersAllV1(token)));
 });
+
 // userProfile
 app.get('/user/profile/v2', (req, res) => {
   const token = String(req.query.token);
   const uId = Number(req.query.uId);
   res.json(userProfileV2(token, uId));
 });
+
+// getUId
 app.post('/other/getUId/v1', (req, res) => {
   const data = req.body;
   res.json(getUId(data.authUserId));
 });
-// channelsListV2
-app.get('/channels/list/v2', (req, res) => {
-  const data = req.query.token as string;
-  res.json(channelsListV2(data));
-});
-// channelsListallV2
-app.get('/channels/listall/v2', (req, res) => {
-  const data = req.query.token as string;
-  res.json(channelsListallV2(data));
-});
-app.post('/channels/create/v2', (req, res) => {
-  const token = String(req.body.token);
-  const name = String(req.body.name);
-  const isPublic = Boolean(req.body.isPublic);
-  res.json(channelsCreateV1(token, name, isPublic));
-});
+
 // userSetnameV1
 app.put('/user/profile/setname/v1', (req, res) => {
   const { token, nameFirst, nameLast } = req.body;
   res.json(userSetnameV1(token, nameFirst, nameLast));
   // console.log(userSetnameV1(token, nameFirst, nameLast));
 });
+
 // userSetemailV1
 app.put('/user/profile/setemail/v1', (req, res) => {
   const { token, email } = req.body;
   res.json(userSetemailV1(token, email));
 });
+
 // userSethandlelV1
 app.put('/user/profile/sethandle/v1', (req, res) => {
   const { token, handleStr } = req.body;
   res.json(userSethandlelV1(token, handleStr));
 });
 
-// channelsListV2
-app.get('/channels/list/v2', (req, res) => {
-  const data = req.query.token as string;
-  res.json(channelsListV2(data));
-});
 
-// channelsListallV2
-app.get('/channels/listall/v2', (req, res) => {
-  const data = req.query.token as string;
-  res.json(channelsListallV2(data));
-});
+
 
 // channelDetailsV2
 app.get('/channel/details/v2', (req, res) => {
